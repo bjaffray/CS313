b49a78e4d173f28b57779c54fdb505187bfee548
bjaffray <jaf15001@byui.edu>
2019-05-08 13:08:01 -0600

    Edits

--- web/group.php
+++ web/group.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello World!</title>
+    <title>Group</title>
 </head>
+
+
+
 <body>
     
-    Hello World!
+<?php
 
+require("nav.php");
+
+?>
+
+Group!
+    
 </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Activity group finder</title>
+</head>
+
+
+
+
+
+
+
+
+<body>
+
 <?php
 
-  phpinfo();
+require("nav.php");
 
 ?>
+
+
+
+  Hello, world!
+</body>
+
+</html>
